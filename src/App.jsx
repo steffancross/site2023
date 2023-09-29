2b3febad5d53899110a18d56d5c8c2f577e90475
Steffan Cross <steffancross@berkeley.edu>
2023-09-28 22:43:43 -0700

    updated npm package to my vers. now all moving on screen can be freezed on click. also all can take hover effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { DvdScreensaver } from "react-dvd-screensaver";
+import { useState } from "react";
+import { DvdScreensaver } from "stffn-react-dvd-screensaver";
 
 function App() {
-  const speed = 3;
+  const speed = 2;
+  const [freeze, setFreeze] = useState(true);
+
+  const toggleFreeze = () => {
+    setFreeze((prev) => !prev);
+  };
 
   return (
     <>
+      <button className="freeze" onClick={toggleFreeze}>
+        click
+      </button>
       <div className="container">
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
+        <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
           <div className="child">ABOUT</div>
         </DvdScreensaver>
-        {/* <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
+        <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
           <div className="child">BLOCKS</div>
         </DvdScreensaver>
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
-          <div className="child">INFINITE DUNGEON CRAWLER</div>
-        </DvdScreensaver>
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
-          <div className="child">AD.HOC</div>
-        </DvdScreensaver>
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
-          <div className="child">LINKEDIN</div>
-        </DvdScreensaver>
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
-          <div className="child">RESUME</div>
-        </DvdScreensaver>
-        <DvdScreensaver className="parent" speed={speed} freezeOnHover={true}>
-          <div className="child">LEGACY SITE</div>
-        </DvdScreensaver> */}
       </div>
     </>
   );
 }
 
 export default App;
+
+// <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
+//   <div className="child">INFINITE DUNGEON CRAWLER</div>
+// </DvdScreensaver>
+// <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
+//   <div className="child">AD.HOC</div>
+// </DvdScreensaver>
+// <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
+//   <div className="child">LINKEDIN</div>
+// </DvdScreensaver>
+// <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
+//   <div className="child">RESUME</div>
+// </DvdScreensaver>
+// <DvdScreensaver className="parent" speed={speed} freezeOnBool={freeze}>
+//   <div className="child">LEGACY SITE</div>
+// </DvdScreensaver>
